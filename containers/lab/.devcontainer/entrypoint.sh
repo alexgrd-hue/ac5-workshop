b7af6e739a807e74c08d2c1f764584b447c4720f
alexgrd-hue <alexigardet@gmail.com>
2026-06-08 09:56:02 +0000

    Add containers/lab with Dockerfile and entrypoint

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -2,12 +2,9 @@
 
 set +e
 
-# on Codespaces this will not work correctly
 if ! ${CODESPACES:-false}; then
-    # Execute command from docker cli if any.
     if [ ${@+True} ]; then
         exec "$@"
-    # Otherwise just enter sh or zsh.
     else
         if [ -f "/bin/zsh" ]; then
             exec zsh
